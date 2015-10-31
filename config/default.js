6c14eb783e707881c47b5476cfce09c9dbbd33af
Marius Büscher <marius.buescher@gmx.de>
2015-10-31 17:05:09 +0100

    Add GitHub authentication

--- config/default.js
+++ config/default.js
@@ -4,6 +4,13 @@
   module.exports = {
     server: {
       port: 3000
+    },
+
+    github: {
+      clientId: 'your-client-id-here',
+      clientSecret: 'your-client-secret-here',
+      encryptionPassword: 'github-encryption-password'
     }
   };
+
 })( module );
--- src/index.js
+++ src/index.js
@@ -10,16 +10,36 @@
     port: Number( process.env.PORT ) || Config.server.port
   } );
 
-  server.register( [], function( err ) {
+  server.register( [
+    require( 'bell' )
+  ], function( err ) {
 
     if( err ) {
         throw err;
     }
 
+    server.auth.strategy('github', 'bell', {
+        provider: 'github',
+        password: Config.github.encryptionPassword,
+        clientId: Config.github.clientId,
+        clientSecret: Config.github.clientSecret,
+        scope: [ 'user:email', 'gist' ],
+        isSecure: false     // Terrible idea but required if not using HTTPS
+    });
+
     server.route( [
-      { method: 'GET', path: '/', handler: function( request, reply ) {
-        reply( 'Hello World' );
-      } },
+      { method: 'GET', path: '/', config: {
+          auth: 'github',
+          handler: function( request, reply ) {
+
+            if (!request.auth.isAuthenticated) {
+              return reply('Authentication failed due to: ' + request.auth.error.message);
+            }
+
+            return reply( 'Authentication successful' );
+          }
+        }
+      }
     ] );
     server.start( function () {
       console.log( 'Server running at:', server.info.uri );
