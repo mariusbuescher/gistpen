611de1e78642b025db3097c1437b72d0d9814d15
Marius Büscher <marius.buescher@gmx.de>
2015-10-31 16:09:35 +0100

    Add Hello World server

---
+++ config/default.js
@@ -0,0 +1,9 @@
+(function( module ) {
+  'use strict';
+
+  module.exports = {
+    server: {
+      port: 3000
+    }
+  };
+})( module );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 (function() {
   'use strict';
 
-  console.log('Hello world!');
-});
+  var Config = require( 'config' );
+  var Hapi = require( 'hapi' );
+
+  var server = new Hapi.Server();
+
+  server.connection( {
+    port: Number( process.env.PORT ) || Config.server.port
+  } );
+
+  server.register( [], function( err ) {
+
+    if( err ) {
+        throw err;
+    }
+
+    server.route( [
+      { method: 'GET', path: '/', handler: function( request, reply ) {
+        reply( 'Hello World' );
+      } },
+    ] );
+    server.start( function () {
+      console.log( 'Server running at:', server.info.uri );
+    } );
+  } );
+})();
